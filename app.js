1388726eec89d62d9d07aa92521045c4afabf379
Ghaalia <Ghaliaals9@gmail.com>
2023-10-24 18:35:47 +0300

    ..

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const accounts = require("./accounts");
 const accountRouter = require("./api/account/routes");
+const connectDB = require("./database");
 
 const app = express();
 app.use(express.json());
 
 app.use("/api", accountRouter);
+
+connectDB();
 
 app.listen(8000, () => {
   console.log("App is running on port 8000");
